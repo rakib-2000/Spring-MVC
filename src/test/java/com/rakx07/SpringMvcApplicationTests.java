57ff464f504d6c0584a10e5017b6ddd2b32891c8
rakx07 <rakib15-13846@diu.edu.bd>
2025-03-22 15:54:56 +0600

    first commit

---
+++ src/test/java/com/rakx07/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakx07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
